61d131ca052ddf8fede152c72005de66f2c8ad78
Azmain Adel <azmainadel47@gmail.com>
2023-08-08 22:48:01 +0600

    Time: 883 ms (88.37%), Space: 27.3 MB (93.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxProfit = 0
-        minPrice = 99999
+        minPrice = float('inf')
 
         for price in prices:
             minPrice = min(minPrice, price)
