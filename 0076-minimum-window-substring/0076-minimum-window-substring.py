19f78e230c1ec8f6a8ba9f4560d199f761cc0c1d
Azmain Adel <azmainadel47@gmail.com>
2023-08-08 15:49:15 +0600

    Time: 172 ms (40.4%), Space: 17.2 MB (23.19%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -4,32 +4,34 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         ans = ''
         min_length = float('inf')
-        
-        required_counter = Counter(t)
-        current_char_counter = Counter()
 
+        required_counter = Counter(t)
         required_chars = len(required_counter)
+
+        current_counter = Counter()
         found_chars = 0
 
         l, r = 0, 0
 
         while r < len(s):
-            current_char_counter[s[r]] += 1
+            current_counter[s[r]] += 1
 
-            if s[r] in required_counter and current_char_counter[s[r]] == required_counter[s[r]]:
+            if s[r] in required_counter and current_counter[s[r]] == required_counter[s[r]]:
                 found_chars += 1
-            
+
             while found_chars == required_chars and l <= r:
                 if (r - l + 1) < min_length:
                     min_length = r - l + 1
                     ans = s[l:r+1]
                 
-                if s[l] in required_counter and current_char_counter[s[l]] == required_counter[s[l]]:
+                if s[l] in required_counter and current_counter[s[l]] == required_counter[s[l]]:
                     found_chars -= 1
 
-                current_char_counter[s[l]] -= 1
+                current_counter[s[l]] -= 1
                 l += 1
-                
+            
             r += 1
-
+        
         return ans
+
+
